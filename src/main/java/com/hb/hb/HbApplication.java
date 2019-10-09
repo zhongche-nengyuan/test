62d14d032aae6a1d3c0825b7fc78fa36559218e9
YUNXIANSHENG <937249331@qq.com>
2019-10-09 11:56:14 +0800

    中车环保

---
+++ src/main/java/com/hb/hb/HbApplication.java
@@ -0,0 +1,13 @@
+package com.hb.hb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbApplication.class, args);
+    }
+
+}
